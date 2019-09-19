62201180e49d3b86b2bcb840be1e7cd3266aa321
nidura <niduraprageeth@gmail.com>
2019-09-19 14:06:46 +0530

    config changes

--- kyc-network/down.sh
+++ kyc-network/down.sh
@@ -3,8 +3,4 @@ docker-compose -f docker-compose.yaml -f docker-compose-ca.yaml down
 # remove orderer block and other channel configuration transactions and certs
 rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config
 # clearContainers
-CONTAINER_IDS=$(docker ps -aq)
-  if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
-    echo "---- No containers available for deletion ----"
-  else
-    docker rm -f $CONTAINER_IDS
+docker rm -f $(docker ps -aq)
